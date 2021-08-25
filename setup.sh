9fb9bbe92da43b958df7b950879f70f334f891d3
romes <rodrigo.m.mesquita@gmail.com>
2021-08-25 08:05:08 +0100

    add setup.sh, updates

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo "TODO: make sure files don't exist yet"
 cd dotfiles/private && make decrypt
+
+# must have programs
+echo "Programs to install:"
+echo "exa"
+echo "pywal (latest version -- pip install https://github.com/dylanaraps/pywal/archive/master.zip)"
