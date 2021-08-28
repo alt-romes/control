ac83bb80085bc70042b56063898b4971f564edd9
romes <rodrigo.m.mesquita@gmail.com>
2021-08-28 21:45:35 +0100

    organize system zero setup

--- system-zero-setup.sh
+++ system-zero-setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# Install Developer tools
+# ----- --- -----
+
+echo "Installing developer command line tools..."
 xcode-select --install
 
-# Install Homebrew
+echo "Installing Homebrew..."
 if ! command -v brew >/dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)";
 fi;
@@ -11,18 +13,18 @@ brew update
 
 BREW_PREFIX=$(brew --prefix)
 
-brew install coreutils
-
-# Install bash v5 with completion
+echo
+echo "Installing latest bash with completion, and setting it as the default shell..."
 brew install bash
 brew install bash-completion@2
 
-# Set bash v5 as the default shell
 if ! grep -Fq "${BREW_PREFIX}/bin/bash" /etc/shells; then
   # brew_prefix/bin/bash must be in the /etc/shells file to be a valid shell to change to with chsh (`man chsh`)
   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
+
+# ----- --- -----
 
 function prompt_optinstall {
     read -r -p "Do you want to install $1? (y/n) "
@@ -36,46 +38,16 @@ function prompt_optinstall {
     fi
 }
 
-echo "Current git version: $(/usr/bin/git --version)"
-prompt_optinstall "a possibly more recent git" && brew install git
+# ----- --- -----
 
-echo "Current vim version: $(/usr/bin/vim --version)"
-echo "(WARNING: The following installation might break pywal)"
-echo "If it does, run 'brew uninstall vim' to attempt a fix"
-prompt_optinstall "a possibly more recent vim (from --HEAD)" && brew install vim --HEAD
+echo
+echo "Installing tools for control..."
+brew install gnupg # privacy/encryptation tools, used to decrypt the private dotfiles
 
-echo "Current ssh version: $(/usr/bin/ssh -V)"
-prompt_optinstall "a possibly more recent openssh" && brew install openssh
+# ----- --- -----
 
-brew install gnupg # privacy/encryptation tools
-
-brew install python
-ln -si /usr/local/bin/python3 /usr/local/bin/python # create a symlink for python to shadow /usr/bin/python
-ln -si /usr/local/bin/pip3 /usr/local/bin/pip # create a symlink to write pip instead of pip3
-
-brew install exa # modern ls replacement
-brew install pass # unix like password manager
-pip install https://github.com/dylanaraps/pywal/archive/master.zip # pywal
-
-prompt_optinstall "translate-shell" && brew install translate-shell
-prompt_optinstall "weechat" && brew install weechat
-
-prompt_optinstall "iterm" && brew install --cask iterm2
-prompt_optinstall "anki" && brew install --cask anki
-prompt_optinstall "vlc" && brew install --cask vlc
-prompt_optinstall "flycut" && brew install --cask flycut
-prompt_optinstall "f.lux" && brew install --cask flux
-prompt_optinstall "lyricsx" && brew install --cask lyricsx
-
-# Fonts
-if prompt_optinstall "the IBM Plex typeface"; then
-    brew tap homebrew/cask-fonts;
-    brew install --cask font-ibm-plex;
-fi;
-
-
-# Set up Control
-
+echo
+echo "Setting up control..."
 echo "To clone the -control- repository, a valid private ssh key is required."
 read -r -p "Do you want to load a valid private ssh key? (y/n) "
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -102,22 +74,84 @@ fi
 
 cd control || exit 1
 git pull
-./control-setup.sh
+source control-setup.sh
 
-# Other
+# ----- --- -----
+
+echo
+echo "Highly recommended installations..."
+
+echo "1) coreutils and updated tools:"
+
+prompt_optinstall "GNU coreutils" && brew install coreutils
+
+echo "Current git version: $(/usr/bin/git --version)"
+prompt_optinstall "a possibly more recent git" && brew install git
+
+echo "Current ssh version: $(/usr/bin/ssh -V)"
+prompt_optinstall "a possibly more recent openssh" && brew install openssh
+
+echo "Current python version: $(/usr/bin/python3 -V)"
+prompt_optinstall "and set up a possibly more recent python" && {
+    brew install python
+    ln -si /usr/local/bin/python3 /usr/local/bin/python # create a symlink for python to shadow /usr/bin/python
+    ln -si /usr/local/bin/pip3 /usr/local/bin/pip # create a symlink to write pip instead of pip3
+}
+
+echo "2) recommended terminal programs:"
+
+echo "(WARNING: 'ls' is set as an alias to 'exa' in .bash_profile, either it's installed or the alias is deleted):"
+prompt_optinstall "exa (modern ls replacement)" && brew install exa # modern ls replacement, 'ls' is set as an alias for it
+prompt_optinstall "pass -- the unix password manager" && brew install pass # unix like password manager
+prompt_optinstall "pywal (to set colors according to the wallpaper)" && pip3 install https://github.com/dylanaraps/pywal/archive/master.zip # pywal
+prompt_optinstall "translate-shell" && brew install translate-shell
+prompt_optinstall "weechat" && brew install weechat
 prompt_optinstall "ticker" && brew install achannarasappa/tap/ticker
-prompt_optinstall "imagemagick (the default pywal backend)" && brew install imagemagick
 
-if prompt_optinstall "the Haskell Platform"; then
-    curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh;
+echo "3) recommended programs:"
+
+prompt_optinstall "iterm" && brew install --cask iterm2
+prompt_optinstall "anki" && brew install --cask anki
+prompt_optinstall "vlc" && brew install --cask vlc
+prompt_optinstall "webtorrent" && brew install --cask webtorrent
+prompt_optinstall "flycut" && brew install --cask flycut
+prompt_optinstall "f.lux" && brew install --cask flux
+prompt_optinstall "lyricsx" && brew install --cask lyricsx
+
+echo "4) recommended fonts:"
+
+if prompt_optinstall "the IBM Plex typeface (used by an iTerm profile)"; then
+    brew tap homebrew/cask-fonts;
+    brew install --cask font-ibm-plex;
 fi;
 
-prompt_optinstall "discord" && brew install --cask discord
-prompt_optinstall "steam" && brew install --cask steam
+# ----- --- -----
+
+echo
+echo "Other recommended instalations..."
+
+prompt_optinstall "imagemagick (the default pywal backend)" && brew install imagemagick
+
+echo "Current vim version: $(/usr/bin/vim --version)"
+echo "(WARNING: The following installation might break pywal):"
+echo "If it does, run 'brew uninstall vim' to attempt a fix"
+prompt_optinstall "a possibly more recent vim (from --HEAD)" && brew install vim --HEAD
+
+prompt_optinstall "the Haskell Platform" && ( curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh )
+prompt_optinstall "rust (and its toolchain)" && ( curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh )
+prompt_optinstall "node" && brew install node
+prompt_optinstall "go" && brew install go
+
+
+# ----- --- -----
 
 echo "Suggested for installation:"
+echo "brew install --cask discord"
+echo "brew install --cask steam"
 echo "brew install tldr"
 
 echo "Vielleicht: Things3, Minecraft, Baba Is You, NVIDIA GeForce"
 
-echo "Complete. Restart your session (on iTerm), and run 'wal -i background.png'"
+# ----- --- -----
+
+echo "Complete. Restart your session (on iTerm). Optionally set colors and background by running 'wal -i background.png'"
