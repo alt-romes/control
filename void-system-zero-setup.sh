fbf8e8a2714c1236492f9cca320e36c826441d23
romes <rodrigo.m.mesquita@gmail.com>
2021-09-10 12:20:21 +0100

    void system zero setup

---
+++ void-system-zero-setup.sh
@@ -0,0 +1,62 @@
+set -e
+echo "System must be connected to the internet through an ethernet cable, or through WiFi with wpa_supplicant (and the dhcpcd service enabled)"
+
+echo "Updating xbps (package manager)"
+sudo xbps-install -u xbps
+
+echo "Updating system"
+sudo xbps-install -Su
+
+echo "XBPS does not restart services when they are updated. To find processes running different versions than are present on disk, use the xcheckrestart tool provided by the xtools package."
+
+
+# Power saving
+echo "Power saving: Installing tlp"
+sudo xbps-install tlp
+echo "Enabling tlp service"
+sudo ln -s /etc/sv/tlp/ /var/service/
+
+
+# Configure network
+echo "Configure network..."
+echo "Set up and download networkmanager (daemon that manages Ethernet, Wifi, ... -- other network management services must be disabled)"
+echo "Downloading NetworkManager"
+echo "If the following fails, run xbps-install -Su again"
+sudo xbps-install NetworkManager
+echo "Disabling dhcpcd service"
+sudo rm /var/service/dhcpcd
+echo "Enabling dbus service (NetworkManager uses it)"
+sudo ln -s /etc/sv/dbus/ /var/service/
+echo "Enabling networkmanager service"
+sudo ln -s /etc/sv/NetworkManager/ /var/service/
+echo "Users of NetworkManager must belong to the network group"
+sudo usermod -a -G network $(whoami)
+echo "NetworkManager is installed. Run nmcli or nmtui to check for a connection"
+
+
+# Graphical session
+echo "Graphical session..."
+echo "Installing Xorg"
+sudo xbps-install xorg
+
+echo "Installing openbox (window manager)"
+sudo xbps-install openbox
+echo "Installing openbox configuration app (obconf)"
+sudo xbps-install obconf
+echo "Installing picom (compositor)"
+sudo xbps-install picom
+echo "Note: picom might still be outdated in relation to rounded corners. In this case, download void-package, edit the picom template version and work with xbps-src (binutils required)"
+
+echo "Installing GNUPG"
+sudo xbps-install gnupg
+echo "TODO: Import keys (see macOS script)"
+
+# Apps
+sudo xbps-install rxvt-unicode # terminal
+sudo xbps-install vim
+sudo xbps-install git
+sudo xbps-install curl
+sudo xbps-install firefox
+
+# Utils
+sudo xbps-install make
