707f72c0796dd667d4ded03c548fef36df3b1514
romes <rodrigo.m.mesquita@gmail.com>
2021-09-18 01:50:12 +0100

    update cheatsheet

--- util/bin/cheatsheet.sh
+++ util/bin/cheatsheet.sh
@@ -8,4 +8,7 @@ sudo qemu-img create -f qcow2 disk.qcow 5G
 # Run voidlinux cdrom image with qemu, 4G RAM, macOS accelerator (hvf), USB on /dev/sdb, with UEFI boot loader (built OVMF for Intel macOS)
 sudo qemu-system-x86_64 -m 4096 -accel hvf -cdrom void-live-x86_64-20210218.iso -hda disk.qcow -hdb /dev/disk2 -pflash control/util/OVMF.fd
 
+# Run UEFI bootable OS from first external drive
+sudo qemu-system-x86_64 -accel hvf -m 4096 -pflash control/util/OVMF.fd -hda /dev/disk2
+
 EOF
