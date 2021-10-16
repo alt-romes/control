3b2773267e80251e537dcec18d7709013678799b
romes <rodrigo.m.mesquita@gmail.com>
2021-10-16 12:33:39 +0100

    tweaks

--- .qemu/Void Linux/start.sh
+++ .qemu/Void Linux/start.sh
@@ -2,11 +2,11 @@
 
 source ../environment.sh
 
-sudo qemu-system-x86_64 \
+qemu-system-x86_64 \
     -accel hvf \
     -m 4G \
     -pflash $FIRMWAREDIR/OVMF.fd \
     -usb -device usb-kbd -device usb-mouse \
-    -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22 \
+    -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::6666-:22 \
     -hda voidlinux.qcow2 \
-    -hdb /dev/rdisk2 \
+    # -hdb /dev/rdisk2 \
