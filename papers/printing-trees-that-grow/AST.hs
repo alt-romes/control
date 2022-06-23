86e1af0070c724f9178ffc61ed258655a152b9f2
romes <rodrigo.m.mesquita@gmail.com>
2022-06-23 22:22:12 +0200

    Continue Watering Trees That Grow

--- papers/printing-trees-that-grow/AST.hs
+++ papers/printing-trees-that-grow/AST.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeFamilies #-}
 module AST where
+
+import GHC.Types (Symbol)
 
 type Var = String
 data Typ = Int
@@ -20,6 +29,29 @@ type family XAbs p
 type family XApp p
 type family XXExpr p
 
-printExpr :: Expr p -> String
-printExpr = \case
-    Lit _ i -> 
+class TTG p (s :: Symbol) t where
+    override :: t -> t
+
+printT :: Typ -> String
+printT = \case
+    Int -> "Int"
+    Fun a b -> "(" <> printT a <> ") -> (" <> printT b <> ")"
+
+printE :: Expr p -> String
+printE = \case
+    Lit _ i -> show i
+    Var _ s -> s
+    Ann _ e t -> "(" <> printE e <> ") :: (" <> printT t <> ")"
+    Abs _ v e -> "λ" <> v <> "." <> printE e
+    App _ f v -> "(" <> printE f <> ") (" <> printE v <> ")"
+    XExpr _ -> ""
+
+instance TTG p "show" (Expr p -> String) => Show (Expr p) where
+    show = override @p @"show" printE
+
+class Pretty p where
+    ppr :: p -> String
+
+instance TTG p "ppr" (Expr p -> String) => Pretty (Expr p) where
+    ppr = override @p @"ppr" printE
+
