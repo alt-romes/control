2d508eea46e8c0968a7523da279a036ff01223f2
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-02-05 19:55:19 +0000

    nix-darwin launchd daemon for hledger ab

--- nix/darwin/macmini.nix
+++ nix/darwin/macmini.nix
@@ -1,5 +1,40 @@
 # Mac Mini M4
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+    # import default.nix from the repo, apply it to the current nixpkgs, and
+    # select the activobank-hs package out.
+    activobank-hs-repo = pkgs.fetchFromGitHub {
+        owner = "alt-romes";
+        repo  = "activobank-hs";
+        rev   = "master";
+        hash  = "sha256-DUfjSGF1fdo9fYeo9uwY5Zpo3fPtFYt47Djvvb/Vfls=";
+    };
+    activobank-hs = (import activobank-hs-repo { nixpkgs = pkgs; }).activobank-hs;
+in
+{
+
+  # user daemons
+  launchd = {
+    user = {
+      agents = {
+        activobank-fetch = {
+           # Use binary from building derivation
+           command = "${activobank-hs}/bin/hledger-activobank";
+           environment = { LC_ALL = "UTF-8"; };
+           # Runs every day at 21:30
+           serviceConfig = {
+               StartCalendarInterval = {
+                 Hour = 21;
+                 Minute = 30;
+               };
+               StandardOutPath   = "/tmp/org.romes.activobank-hs.out.log";
+               StandardErrorPath = "/tmp/org.romes.activobank-hs.err.log";
+           };
+        };
+      };
+    };
+  };
+
   homebrew = {
 
     brews = [
