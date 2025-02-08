e6fd225cbd664b80666fcbab1a7644c3d92d8d84
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-02-08 15:03:20 +0000

    finances re-enable

--- nix/darwin/macmini.nix
+++ nix/darwin/macmini.nix
@@ -2,9 +2,13 @@
 { pkgs, config, ... }:
 {
 
-  imports = [ ../../finances/finances.nix  ];
+  imports = [
+    ../../finances/finances.nix
+  ];
 
   finances = {
+    enable = true;
+    daemons.enable = true;
     personal.ledger = "/Users/romes/control/finances/2024.journal";
     mogbit.ledger = "/Users/romes/control/finances/mogbit/2024.journal";
   };
