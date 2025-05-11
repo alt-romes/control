d607a103ec7d9834f249ff3b7bbfe56bce214179
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-05-11 23:13:53 +0100

    Wrappers around Shortcuts;
    The shortcuts need to be available in the app, but I'm keeping a backup copy in this folder of each. We try to load them automatically if they are not found. The unfortunate bit is that these backups may fall out of date with the original thing, but then again they aren't meant to change much

---
+++ nix/darwin/shortcuts/shortcuts.nix
@@ -0,0 +1,44 @@
+{ lib, pkgs, ... }:
+let
+  # Get all the *.shortcut files from the folder
+  shortcutsDir = ./.;
+  shortcutFiles = builtins.filter (f: lib.hasSuffix ".shortcut" f)
+                    (builtins.attrNames (builtins.readDir shortcutsDir));
+
+  mkShortcutDeriv = fileName:
+    let
+      # Drop .shortcut suffix
+      shortcutName = "romes: ${lib.removeSuffix ".shortcut" fileName}";
+      binName = lib.strings.toLower (lib.strings.sanitizeDerivationName shortcutName);
+    in
+    pkgs.writeShellScriptBin binName ''
+        if ! shortcuts list | grep -Fxq "${shortcutName}"; then
+          echo "Shortcut '${shortcutName}' not found. Trying to install from source..." >&2
+          echo "Installing from backup version: \"${shortcutsDir}/${fileName}\"."
+          echo "Please confirm this shortcut is up to date with what it should be doing."
+          echo "--------------------------------------------------------------------------------"
+          echo "IMPORTANT: Please rename the shortcut to add the 'romes: ' suffix."
+          echo "--------------------------------------------------------------------------------"
+          open "${shortcutsDir}/${fileName}"
+          read -p "Press any key to continue" -n 1 -s
+        fi
+
+        if [ -z "$1" ]; then
+          shortcuts run "${shortcutName}"
+        else
+          if [ -f "$1" ]; then
+            shortcuts run "${shortcutName}" -i "$1"
+          else
+            SHORTCUT_INPUT_TMP="$(mktemp -d)/input"
+            echo "$1" > "$SHORTCUT_INPUT_TMP"
+            shortcuts run "${shortcutName}" -i "$SHORTCUT_INPUT_TMP"
+          fi
+        fi
+    '';
+in
+{
+  inherit mkShortcutDeriv;
+} // builtins.listToAttrs (map (fileName: {
+    name = lib.strings.sanitizeDerivationName (lib.removeSuffix ".shortcut" fileName);
+    value = mkShortcutDeriv fileName;
+  }) shortcutFiles)
