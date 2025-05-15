74a3cf00736404ae79a1096ca6162cb82d6ba1bd
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-05-15 21:58:23 +0100

    mbp: Wireguard fixes

--- nix/darwin/mbp.nix
+++ nix/darwin/mbp.nix
@@ -34,20 +34,25 @@
   age.secrets.wireguard-mbp.file = ../../secrets/wireguard-mbp.age;
 
   # Wireguard client
+  # ---
+  # It seems like this doesn't get applied properly while wg is up? possibly
+  # because WG is running or the interface is already set up. Use
+  # @sudo wg-quick down wg0@ to bring it down before applying the nix config.
+  #
+  # If nothing works, comment out @wg0@, rebuild, uncomment, rebuild.
   networking.wg-quick.interfaces = {
     wg0 = {
       autostart = true;
-      address = [ "10.10.10.2/32" "fc10:10:10::2/128" ];
+      address = [ "10.0.0.2/32" ];
       privateKeyFile = config.age.secrets.wireguard-mbp.path; # for kmpmnUIFpfS4mdOzi7RlGShhSqOcelwIDG+/8mJUAzM=
       peers = [
         # wireguard-macmini
         {
           publicKey = "hroqsfMWbBsCYhZTiPLNeE/3AK+AdBV3Zn16EJspPX8=";
-          allowedIPs = [ "0.0.0.0/0" "::/0" ]; # allow server to be anywhere
+          allowedIPs = [ "10.0.0.1/24" ]; # allow server to be anywhere
           endpoint = "alt-romes.duckdns.org:55902"; # the dynamic dns we set up with duckdns
         }
       ];
     };
   };
-
 }
