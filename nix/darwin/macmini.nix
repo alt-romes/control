d214a78206a8387ed9ef9ec597e7cdce9d43495d
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-05-15 21:56:16 +0100

    mm: Wireguard tweaks

--- nix/darwin/macmini.nix
+++ nix/darwin/macmini.nix
@@ -54,17 +54,23 @@
   };
 
   # Wireguard
+  # ---
+  # It seems like this doesn't get applied properly while wg is up? possibly
+  # because WG is running or the interface is already set up. Use
+  # @sudo wg-quick down wg0@ to bring it down before applying the nix config.
+  #
+  # If nothing works, comment out @wg0@, rebuild, uncomment, rebuild.
   networking.wg-quick.interfaces = {
     wg0 = {
       autostart = true;
-      address = [ "10.10.10.1/24" "fc10:10:10::1/64" ];
+      address = [ "10.0.0.1/24" ];
       listenPort = 55902;
       privateKeyFile = config.age.secrets.wireguard-macmini.path; # for hroqsfMWbBsCYhZTiPLNeE/3AK+AdBV3Zn16EJspPX8=
       peers = [
         # wireguard-mbp
         {
           publicKey = "kmpmnUIFpfS4mdOzi7RlGShhSqOcelwIDG+/8mJUAzM=";
-          allowedIPs = [ "10.10.10.2/32" "fc10:10:10::2/128" ];
+          allowedIPs = [ "10.0.0.2/32" ];
         }
       ];
     };
