03da401d04e72c29aab0282bc57a2d5324dab240
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-09-03 23:49:12 +0100

    Few more tweaks

--- nix/darwin/common.nix
+++ nix/darwin/common.nix
@@ -76,6 +76,7 @@
     # reproducibility / idempotence of brew formulas and casks, as they're not
     # pinned in any meaningful way. This applies mostly to Casks anyway :)
     onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
   };
 
   # 1Password CLI
@@ -101,7 +102,7 @@
 
       g = "git";
       httpserver = "nix-shell -p python3 --run 'python -m http.server 25565'";
-      darwin-nix-switch = "darwin-rebuild switch --flake '/Users/romes/control/nix/.?submodules=1'"; # submodules=1 is needed because some modules of the system are in git submodules (such as finances.nix)
+      darwin-nix-switch = "sudo darwin-rebuild switch --flake '/Users/romes/control/nix/.?submodules=1'"; # submodules=1 is needed because some modules of the system are in git submodules (such as finances.nix)
       # ghc-nix = "nix develop git+https://gitlab.haskell.org/ghc/ghc.nix";
       ghc-nix = "nix-shell -p haskell.compiler.ghc910 haskellPackages.alex haskellPackages.happy autoconf automake python3 gmp zlib";
     };
