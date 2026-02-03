d4affbcf165999401d4e84930c0b2c64935e69c3
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-02-03 10:52:16 +0000

    Linux VM

---
+++ nix/linux/dev-vm/configuration.nix
@@ -0,0 +1,79 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [ <nixpkgs/nixos/modules/profiles/all-hardware.nix> ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "dev-vm";
+
+  # For remote nixos-rebuild
+  nix.settings.trusted-users = [ "romes" ];
+
+  # Configure network connections interactively with nmcli or nmtui.
+  # networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Portugal";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # hardware.graphics.enable = true;
+  # services.seatd.enable = true;
+  # services.dbus.enable = true;
+
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.romes = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIKdREVP76ISSwCnKzqMCeaMwgETLtnKqWPF7ORZSReZ romes@world"
+    ];
+    extraGroups = [ "wheel" "networkmanager" "video" "input" "seat" ]; # Enable ‘sudo’ for the user.
+    packages = [ ];
+    uid = 1000;
+  };
+
+  programs.firefox.enable = true;
+
+  # $ nix search <package>
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+
+  services.openssh.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
+
