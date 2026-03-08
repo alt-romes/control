07de26d0aaa75f01bbc8ce3494cbb3a520737915
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-03-08 19:10:01 +0000

    If we use .localhost names, then we don't need the local DNS at all, just the reverse proxy

--- nix/darwin/modules/local-proxy.nix
+++ nix/darwin/modules/local-proxy.nix
@@ -30,27 +30,21 @@ in
     };
   };
 
-  config = {
+  config = lib.mkIf cfg.enable {
 
-    # 1. Use our local DNS Resolver
-    # These two options basically amount to calling for each knownNetworkServices:
-    # `networksetup -setdnsservers <knownNetworkService> <dns servers>
-    networking.knownNetworkServices = [ "Wi-Fi" "Ethernet" ];
-    networking.dns = [ "127.0.0.1" ];
+    assertions = [
+      {
+        assertion = lib.all
+          (name: lib.hasSuffix ".localhost" name)
+          (lib.attrNames cfg.hosts);
+        message = "All local-proxy host names must end in .localhost, since we don't do DNS mapping, only reverse proxy on localhost (and .localhost names are automatically redirected to 127.0.01). Offending entries: "
+          + lib.concatStringsSep ", "
+              (lib.filter (n: !lib.hasSuffix ".localhost" n) (lib.attrNames cfg.hosts));
+      }
+    ];
 
-    # 2. The local DNS Resolver (dnsmasq)
-    services.dnsmasq = {
-      enable = true;
-
-      addresses = lib.mapAttrs (_name: entry: entry.host) cfg.hosts;
-
-      # empty list means use the servers from /etc/resolv.conf as a fallback
-      servers = [];
-    };
-
-    # 3. An HTTPS Reverse Proxy: DNS maps names to IPs, we need a way of mapping
-    # names to specific ports too
-    # (currently this is a custom module)
+    # "<name>.localhost" URLs are mapped to 127.0.0.1.
+    # We use a reverse proxy to map the name to a particular host:port
     services.caddy = {
       enable = true;
       virtualHosts = lib.mapAttrs
