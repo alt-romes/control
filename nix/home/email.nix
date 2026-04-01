3c77401db20f6e8cb7f23a97ec81d690dbd4927e
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-04-01 21:21:24 +0100

    Email tweaks

--- nix/home/email.nix
+++ nix/home/email.nix
@@ -2,18 +2,27 @@
 {
   programs.neomutt.enable = true; # Email client
 
-  programs.mbsync.enable = true;  # Email downloader
-  programs.msmtp.enable = true;   # Email sender
-  programs.notmuch.enable = true; # Email indexer (for searching)
+  programs.mbsync.enable = true; # Email downloader
+  programs.msmtp.enable = true;  # Email sender
+  programs.notmuch = {           # Email indexer (for searching)
+    enable = true;
+    hooks = {
+      preNew = "mbsync --all";
+    };
+  };
 
   accounts.email.accounts = {
     mogbit = {
       address = "rodrigo@mogbit.com";
+      aliases = [
+        "rodrigo@kanjideck.com"
+      ];
       imap.host = "mail.mogbit.com";
       mbsync = {
         enable = true;
         create = "maildir";
       };
+      neomutt.enable = true;
       msmtp.enable = true;
       notmuch.enable = true;
       primary = true;
