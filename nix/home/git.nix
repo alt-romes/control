0b9c361fc15acd2070b530e760f689c712e351d9
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-04-08 08:26:13 +0100

    fix git cnfig warnings

--- nix/home/git.nix
+++ nix/home/git.nix
@@ -2,12 +2,13 @@
 
   programs.git = {
     enable = true;
-    userName = "Rodrigo Mesquita";
-    userEmail = "rodrigo.m.mesquita@gmail.com";
     signing.key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIKdREVP76ISSwCnKzqMCeaMwgETLtnKqWPF7ORZSReZ";
     signing.signByDefault = true;
 
-    extraConfig = {
+    settings = {
+      user.name = "Rodrigo Mesquita";
+      user.email = "rodrigo.m.mesquita@gmail.com";
+
       gpg.format = "ssh";
 
       push.default = "current";
@@ -25,27 +26,26 @@
 
       url."git@github.com:".insteadOf = "https://github.com";
       url."git@gitlab.com:".insteadOf = "https://gitlab.com";
+
+      alias.fixup = "!git log -n 50 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --fixup";
     } // lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
       "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-    };
-
-    aliases = {
-      fixup = "!git log -n 50 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --fixup";
-    }; 
-
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        side-by-side = true;
-        light = false;
-      };
     };
 
     # Global ignores
     ignores =
       [ ".DS_Store" "dist-newstyle/" "__pycache__" ".idea" ".vim/undofiles/%*"
         "*.orig" "*.swp" "*.class" "*.aux" "*.log" "*.out" "*.hi" "*.o" "tags" ];
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      side-by-side = true;
+      light = false;
+    };
   };
 
 }
