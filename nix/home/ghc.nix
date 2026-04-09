d8ef71e6bd73a27b91062a03ae64d2ade47ed4d2
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-04-09 15:20:39 +0100

    enable completion for hadrian-util

--- nix/home/ghc.nix
+++ nix/home/ghc.nix
@@ -2,11 +2,23 @@
 { pkgs, inputs, ... }:
 let
   hadrianUtil = inputs.hadrian-util.packages.${pkgs.system}.default;
+  hadrianUtilZshCompletion = pkgs.runCommandLocal "hadrian-util-zsh-completion" {
+    nativeBuildInputs = [ hadrianUtil ];
+  } ''
+    mkdir -p $out/share/zsh/site-functions
+    ${hadrianUtil}/bin/hadrian-util --zsh-completion-script ${hadrianUtil}/bin/hadrian-util > $out/share/zsh/site-functions/_hadrian-util
+  '';
 in
 {
   home.packages = [
     hadrianUtil
+    hadrianUtilZshCompletion
   ];
 
-  programs.zsh.shellAliases.hu = "hadrian-util";
+  programs.zsh = {
+    shellAliases.hu = "hadrian-util";
+    initContent = ''
+      compdef _hadrian-util hu=hadrian-util
+    '';
+  };
 }
