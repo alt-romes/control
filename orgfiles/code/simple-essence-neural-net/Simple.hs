0476acde2a180133204acbdd9b96b3fc9e54c165
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-04-21 22:13:44 +0100

    Reverse mode and train neural net successfully

--- orgfiles/code/simple-essence-neural-net/Simple.hs
+++ orgfiles/code/simple-essence-neural-net/Simple.hs
@@ -1,67 +1,85 @@
 {- cabal:
   build-depends: base, random
 -}
-{-# LANGUAGE GHC2024, BlockArguments, TypeFamilies, UndecidableInstances, AllowAmbiguousTypes #-}
+{-# LANGUAGE GHC2024, BlockArguments, TypeFamilies, UndecidableInstances, AllowAmbiguousTypes, NoMonomorphismRestriction #-}
+import Control.Monad (foldM) -- eh
 import GHC.TypeNats
 import Prelude hiding (id, (.))
 import Control.Category
 import System.Random
 
 -- | Differentiable+ function
-newtype a :-> b = D { (#) :: a -> (b, a -> b) }
+newtype a :-> b = D { (#) :: a -> (b, a <-- b) }
+
+newtype a <-- b = Dual { runDual :: b -> a } -- look at "Transposition (Fig 11)" in "You Only Linearize Once" for how to invert mathematical operations
 
 -- | The derivative of every linear function is itself, everywhere
-linear :: (a -> b) -> (a :-> b)
-linear f = D (\a -> (f a, f))
+linear :: (a -> b) -> (a <-- b) -> (a :-> b)
+linear f fd = D (\a -> (f a, fd))
 
 -- | Differentiable+ functions form a Category
 instance Category (:->) where
-  id = linear (\x -> x)
+  id = linear id (Dual id)
   g . f = D \a -> -- chain rule
-    let (b, f') = f # a
-        (c, g') = g # b
-     in (c, \x -> g' (f' x))
+    let (b, Dual f') = f # a
+        (c, Dual g') = g # b
+     in (c, Dual (f' . g') {- invert ! -})
 
 -- Monoidal
 type (×) = (,)
 (×) :: (a :-> c) -> (b :-> d) -> ((a×b) :-> (c×d))
 f × g = D \(a,b) -> -- paralell composition
-  let (c, f') = f # a
-      (d, g') = g # b
-   in ((c,d), \(x,y) -> (f' x, g' y))
+  let (c, Dual f') = f # a
+      (d, Dual g') = g # b
+   in ((c,d), Dual \(x,y) -> (f' x, g' y))
 
--- Cartesian (algebraically, but not substructurally, linear)
-exl = linear fst
-exr = linear snd
-dup = linear (\x -> (x,x))
+-- Cartesian
+exl = linear fst (Dual $ \x -> (x, 0))          -- TDropLin
+exr = linear snd (Dual $ \x -> (0, x))          -- TDropLin
+dup = linear (\x -> (x,x)) (Dual $ uncurry (+)) -- TLinDup
 
-scale y = \dx -> dx*y -- (linearly) scale argument
+scale y = Dual \dx -> dx*y -- (linearly) scale argument by fixed y (TLinMul)
 --------------------------------------------------------------------------------
-neg  = linear negate
-add  = linear (uncurry (+))
-mul  = D (\(x,y) -> (x*y, \(dx,dy) -> dx*y + dy*x))
-rec  = D (\x -> (recip x, scale (-1 / x^2)))
-exp' = D (\x -> let e = exp x in (e, scale e))
-log' = D (\x -> let l = log x in (l, scale (-1/l)))
+neg  = linear negate (scale (-1))
+add  = linear (uncurry (+)) (Dual $ \x -> (x,x))
+mul  = D \(x,y) -> (x*y, Dual \df -> (df*y,df*x))
+rec  = D \x -> (recip x, scale (-1 / x^2))
+exp' = D \x -> let e = exp x in (e, scale e)
+log' = D \x -> let l = log x in (l, scale (-1/l))
 
 (+>) :: Num a => a -> (a :-> a)
-(+>) k = D \x -> (k+x, id) -- adds constant number
+(+>) k = D \x -> (k+x, Dual id) -- adds constant number
 
 sigmoid = rec . (1 +>) . exp' . neg
 --------------------------------------------------------------------------------
+-- Example
+sqr = mul . dup
+sqrMag = add . (sqr × sqr)
+
+-- gradient is [2x 2y]
+sqrMagGrad x y = runDual (snd (sqrMag # (x,y))) 1 {- seed for the output == 1, returns gradient vector -}
+--------------------------------------------------------------------------------
+-- Neural Net
 type family R n where
   R 1 = Double
   R n = Double × R (n-1)
+
+instance (Num a, Num b) => Num (a, b) where
+  -- another hacky... this allows us to use `dup` and `exl/r` below; and allows math on tuples to train
+  fromInteger x = (fromInteger x, fromInteger x)
+  (+) (a, b) (c, d) = (a+c, b+d)
+  negate (a, b) = (negate a, negate b)
 
 -- ugly... how to avoid this completely?
 class Layer a where
   weightedSum :: a -> a :-> R 1
   weightedSum' :: (a × a) :-> R 1
 instance Layer Double where
-  weightedSum x = linear (* x)
+  weightedSum x = linear (*x) (scale x)
   weightedSum' = mul
-instance Layer b => Layer (Double, b) where
-  weightedSum (x, xs) = add . (linear (* x) × weightedSum xs) . (exl × exr) . dup
+
+instance (Num b, Layer b) => Layer (Double, b) where
+  weightedSum (x, xs) = add . (linear (*x) (scale x) × weightedSum xs)
   weightedSum' :: ((Double, b) × (Double, b)) :-> R 1
   weightedSum' = add . (mul × weightedSum') . ((exl × exl) × (exr × exr)) . dup
 
@@ -92,6 +110,23 @@ cost (p:ps) = normalize . foldl' (\acc x -> add . (cost1 x × acc) . dup) (cost1
 examples :: [(R 2, R 1)]
 examples = [((0,0),0), ((0,1),1), ((1,0),1), ((1,1),0)]
 
+step :: Int -> (L1W, R 4) -> IO (L1W, R 4)
+step i weights = do
+  putStrLn $ "Step: " ++ show i
+  let (r, Dual grad) = cost examples # weights
+  putStrLn $ "Cost: " ++ show r
+  pure $ weights + grad (-1)
+
+train :: IO ()
+train = do
+  initialWeights <- randomIO @(L1W, R 4)
+  finalWeights <- foldM (flip step) initialWeights [0..100000]
+  putStrLn "Neural net result on examples:"
+  print $ map (\ex -> fst (xorNet (fst ex) # finalWeights)) examples
+  putStrLn "Expected results:"
+  print (map snd examples)
+  -- cost examples # weights
+
 -- TODO: Neural Networks made moderately complex:
 --  do more complicated version which uses size-indexed vecs,
 --  and which uses Matrix multiplication by making `L a b` by a data family
@@ -104,3 +139,7 @@ examples = [((0,0),0), ((0,1),1), ((1,0),1), ((1,1),0)]
 --  - We don't use proper linear types -o (see connection between substructural
 --    and algebraic linearity in "You only linearize once")
 --  - We assume the tangent space matches the (co)domain space
+--
+-- We could do an linear algebraically module based on linear substructurally
+-- by making multiplication receive only 1 linear argument (the other is
+-- unrestricted) and making addition work normally OK)
