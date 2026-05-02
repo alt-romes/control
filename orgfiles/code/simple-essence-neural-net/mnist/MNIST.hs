bdb95b3d8e0f2d0474c89ff5c8046cdcba51d52c
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-02 09:11:42 +0100

    UV tests

--- orgfiles/code/simple-essence-neural-net/mnist/MNIST.hs
+++ orgfiles/code/simple-essence-neural-net/mnist/MNIST.hs
@@ -1,14 +1,14 @@
 {- cabal:
  build-depends: base, random, bytestring
 -}
-{-# LANGUAGE GHC2024, OverloadedLists #-}
+{-# LANGUAGE GHC2024, OverloadedLists, Strict #-}
 import Prelude hiding (id, (.))
 import Control.Category
 import Control.Monad
 import System.Random.Stateful
 import qualified Data.ByteString as BS
 import qualified Data.Vector as V
-import GHC.Base (build)
+import qualified Data.Vector.Unboxed as UV
 
 newtype a :-> b = D    { (#)  :: a -> (b, a <-- b) }
 newtype a <-- b = Dual { (<|) :: b -> a            }
@@ -35,56 +35,54 @@ exp'      = D $ \x -> let e = exp x in (e, scale e)
 sqrt'     = D $ \x -> let r = sqrt x in (r, scale (r*(1/2)))
 --------------------------------------------------------------------------------
 dupI join n = linear (\x -> V.replicate n x) (Dual join)
-crossI fs = D $ \as -> let (bs, bsas) = V.unzip (V.zipWith (#) fs as) in (bs, Dual (V.zipWith (<|) bsas))
-sumI      = D $ \xs -> (sum xs, Dual (\x -> V.replicate (length xs) x))
-hadamard  = D $ \(ss, xs) -> (ss .*. xs, Dual (\dfs -> (xs .*. dfs, ss .*. dfs))) where (.*.) = V.zipWith (*)
+crossI :: V.Vector (b :-> a) -> UV.Vector b :-> UV.Vector a
+crossI fs = D $ \as -> let (bs, bsas) = V.unzip (V.zipWith (#) fs (UV.convert as)) in (UV.convert bs, Dual (UV.convert . V.zipWith (<|) bsas . UV.convert))
+sumI      = D $ \xs -> (sum xs, Dual (\x -> UV.replicate (length xs) x))
+hadamard  = D $ \(ss, xs) -> (ss .*. xs, Dual (\dfs -> (xs .*. dfs, ss .*. dfs))) where (.*.) = UV.zipWith (*)
 fixed f a = D $ \b -> let (c, Dual d) = f # (a, b) in (c, Dual (snd . d))
-cons    x = D $ \xs -> (x `V.cons` xs, Dual (\dxs -> V.drop 1 dxs)) -- add a constant number to head of Vec. All weight vecs have leading biases
+cons    x = D $ \xs -> (x `UV.cons` xs, Dual (\dxs -> UV.drop 1 dxs)) -- add a constant number to head of Vec. All weight vecs have leading biases
 --------------------------------------------------------------------------------
 sigmoid  = rec . (1 +>) . exp' . neg -- 1/(1+exp(-x))
-neuron :: (V.Vector Double, V.Vector Double) :-> Double
 neuron   = sigmoid . (sumI . hadamard) . (cons 1 × id)
-l2 = crossI (V.replicate 10 neuron) . (linear (\(xs,ys) -> V.zipWith (,) xs ys) (Dual V.unzip))
-l1 :: V.Vector Double -> V.Vector (V.Vector Double) :-> V.Vector Double
+l2 :: (V.Vector (UV.Vector Double), V.Vector (UV.Vector Double)) :-> UV.Vector Double
+l2 = crossI (V.replicate 10 neuron) . (linear (\(xs,ys) -> UV.convert $ V.zipWith (,) xs ys) (Dual (UV.convert V.unzip)))
 l1 i = crossI (V.replicate 300 (fixed neuron i))
-mnistNet :: V.Vector Double -> (V.Vector (V.Vector Double), V.Vector (V.Vector Double)) :-> V.Vector Double
-mnistNet i = l2 . ((dupI (V.foldr1 (V.zipWith (+))) 10 . l1 i) × id) where n = fixed neuron i
-cost :: [(V.Vector Double, V.Vector Double)]
-     -> (V.Vector (V.Vector Double), V.Vector (V.Vector Double)) :-> Double
+mnistNet :: UV.Vector Double -> Weights :-> UV.Vector Double
+mnistNet i = l2 . ((dupI (V.foldr1 (UV.zipWith (+))) 10 . l1 i) × id) where n = fixed neuron i
+cost :: [(UV.Vector Double, UV.Vector Double)] -> Weights :-> Double
 cost  ps = sumI . crossI (V.map cost1 (V.fromList ps)) . dupI sum (length ps)
-  where cost1 (i, o) = sqrt' . sumI . crossI (V.replicate 10 sqr) . (V.map (*(-1)) o +>) . mnistNet i
+  where cost1 (i, o) = sqrt' . sumI . crossI (V.replicate 10 sqr) . what o . mnistNet i
+        cost1 :: (UV.Vector Double, UV.Vector Double) -> Weights :-> Double
         sqr :: Double :-> Double
         sqr = mul . dup
+        what :: UV.Vector b -> UV.Vector b :-> UV.Vector b
+        what o = (UV.map (*(-1)) o +>)
 
-step :: [(V.Vector Double, V.Vector Double)]
-     -> Int
-     -> (V.Vector (V.Vector Double), V.Vector (V.Vector Double))
-     -> IO (V.Vector (V.Vector Double), V.Vector (V.Vector Double))
 step examples (i :: Int) weights = do
   let (r, Dual grad) = cost examples # weights
   putStrLn $ "Cost(" ++ show i ++ "): " ++ show r
   pure $ weights + grad (-10)
 
 main = do
-  images <- map (fromIntegral @_ @Double) . BS.unpack <$> BS.readFile "train-images.idx3-ubyte"
-  labels <- map (fromIntegral @_ @Double) . BS.unpack <$> BS.readFile "train-labels.idx1-ubyte"
-  let examples :: [(V.Vector Double, V.Vector Double)]
-      examples = zipWith (,) (map V.fromList $ chunksOf 784 images) (map V.singleton labels)
-  initialWeights <- (,) <$> V.mapM (const $ V.replicateM 785 $ randomM globalStdGen) [1..300] <*> V.mapM (const $ V.replicateM 301 $ randomM globalStdGen) [1..10]
+  rawImages <- BS.drop 16 <$> BS.readFile "train-images.idx3-ubyte"
+  rawLabels <- BS.drop 8  <$> BS.readFile "train-labels.idx1-ubyte"
+  let n      = BS.length rawLabels
+      imgVec :: UV.Vector Double
+      imgVec  = UV.generate (n * 784) (\i -> fromIntegral (BS.index rawImages i))
+      examples :: [(UV.Vector Double, UV.Vector Double)]
+      examples = take 100 [ ( UV.generate 784 (\j -> imgVec UV.! (i * 784 + j))
+                   , UV.singleton (fromIntegral (BS.index rawLabels i))
+                   )
+                 | i <- [0..n-1] ]
+  initialWeights <- (,) <$> V.mapM (const $ UV.replicateM 785 $ randomM globalStdGen) [1..300] <*> V.mapM (const $ UV.replicateM 301 $ randomM globalStdGen) [1..10]
   finalWeights   <- V.foldl' (\acc i -> acc >>= step examples i) (pure initialWeights) [0..1000]
   putStrLn $ "Neural net results: " ++ show (map (\(e,_) -> fst (mnistNet e # finalWeights)) examples)
   putStrLn $ "Expected results:   " ++ show (map snd examples)
 
-type Weights = (V.Vector (V.Vector Double), V.Vector (V.Vector Double))
+type Weights = (V.Vector (UV.Vector Double), V.Vector (UV.Vector Double))
 instance Num Weights where
-  fromInteger x' = (V.replicate 300 (V.replicate 785 x), V.replicate 10 (V.replicate 301 x)) where x = fromInteger x'
-  (w1, w2) + (w3, w4) = (V.zipWith (V.zipWith(+)) w1 w3, V.zipWith (V.zipWith(+)) w2 w4)
-instance Num (V.Vector Double) where
+  fromInteger x' = (V.replicate 300 (UV.replicate 785 x), V.replicate 10 (UV.replicate 301 x)) where x = fromInteger x'
+  (w1, w2) + (w3, w4) = (V.zipWith (UV.zipWith(+)) w1 w3, V.zipWith (UV.zipWith(+)) w2 w4)
+instance Num (UV.Vector Double) where
   -- the negate/fromInteger 0/fromInteger -1 was causing such a weird loop (also the `sum` from `dupI` needing a zero..) wow.
-  (+) = V.zipWith (+)
-chunksOf :: Int -> [e] -> [[e]]
-chunksOf i ls = map (take i) (build (splitter ls))
- where
-  splitter :: [e] -> ([e] -> a -> a) -> a -> a
-  splitter [] _ n = n
-  splitter l c n = l `c` splitter (drop i l) c n
+  (+) = UV.zipWith (+)
