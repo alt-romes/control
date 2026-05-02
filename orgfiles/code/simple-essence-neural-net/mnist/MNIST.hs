f2d9578117a5b3e5e0b63ddd9528ecd41f030283
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-03 00:20:37 +0100

    Allocating much less with specialized weighted sum. 100GB.
    Using V.map (*dfs) xs rather than V.zipWith (*) xs (V.replicate dfs) surprisingly consumes 400GB more. Stick with zipWith (*) . V.replicate

--- orgfiles/code/simple-essence-neural-net/mnist/MNIST.hs
+++ orgfiles/code/simple-essence-neural-net/mnist/MNIST.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE GHC2024, TypeAbstractions, Strict, OverloadedStrings,
              DeriveTraversable, DeriveGeneric, MultiParamTypeClasses,
              FlexibleInstances, LambdaCase, TypeFamilies, NoMonomorphismRestriction #-}
-{-# OPTIONS_GHC -fpolymorphic-specialisation -fspecialise-aggressively #-}
+{-# OPTIONS_GHC -fpolymorphic-specialisation -fspecialise-aggressively -ddump-simpl -ddump-to-file -dsuppress-all #-}
 import Prelude hiding (id, (.))
 import Control.Category
 import Control.Monad
@@ -47,12 +47,12 @@ neg       = linear negate (scale (-1))
 mul       = D $ \(x,y) -> (x*y, Dual (\df -> (df*y,df*x)))
 rec       = D $ \x -> (recip x, scale (-1 / x^2))
 exp'      = D $ \x -> let e = exp x in (e, scale e)
-{-# NOINLINE dup #-}
-{-# NOINLINE neg #-}
-{-# NOINLINE (+>) #-}
-{-# NOINLINE mul #-}
-{-# NOINLINE rec #-}
-{-# NOINLINE exp' #-}
+{-# INLINE dup #-}
+{-# INLINE neg #-}
+{-# INLINE (+>) #-}
+{-# INLINE mul #-}
+{-# INLINE rec #-}
+{-# INLINE exp' #-}
 -- pow  k    = D $ \x -> (x^k, scale (k*x^(k-1))) -- (^) only works for integral exponents
 --------------------------------------------------------------------------------
 dupI :: KnownNat n => (V.Vector n a -> a) -> a :-> V.Vector n a
@@ -64,19 +64,18 @@ weightedSum :: forall n c. (KnownNat n, Num c)
             => (V.Vector n c, V.Vector n c) :-> c
 weightedSum = D $ \(ss, xs) ->
   let tot = foldl' (\acc i -> acc + (V.unsafeIndex ss i * V.unsafeIndex xs i)) 0 [0..V.length ss-1]
-   in (tot, Dual (\dfs -> let dfsV = V.replicate dfs in (xs .*. dfsV, ss .*. dfsV)))
-  where (.*.) = V.zipWith (*)
+   in (tot, Dual (\dfs -> (V.zipWith (*) xs (V.replicate dfs), V.zipWith (*) ss (V.replicate dfs))))
 fixed f a = D $ \b -> let (c, Dual d) = f # (a, b) in (c, Dual (snd . d))
 cons :: a -> V.Vector n a :-> V.Vector (1 + n) a
 cons    x = D $ \xs -> (x `V.cons` xs, Dual (\dxs -> V.drop @1 dxs)) -- add a constant number to head of Vec. All weight vecs have leading biases
 zip' = linear (\(xs,ys) -> V.zipWith (,) xs ys) (Dual V.unzip)
-{-# NOINLINE dupI #-}
-{-# NOINLINE crossI #-}
-{-# NOINLINE sumI #-}
-{-# NOINLINE weightedSum #-}
-{-# NOINLINE fixed #-}
-{-# NOINLINE cons #-}
-{-# NOINLINE zip' #-}
+{-# INLINE dupI #-}
+{-# INLINE crossI #-}
+{-# INLINE sumI #-}
+{-# INLINE weightedSum #-}
+{-# INLINE fixed #-}
+{-# INLINE cons #-}
+{-# INLINE zip' #-}
 --------------------------------------------------------------------------------
 sigmoid  = {-# SCC sigmoid #-} rec . (1 +>) . exp' . neg -- 1/(1+exp(-x))
 softmax :: forall n a. (KnownNat n, Floating a) => V.Vector n a :-> V.Vector n a
@@ -86,17 +85,17 @@ l2       = {-# SCC l2 #-} softmax . crossI (V.replicate @NOut neuron) . zip'
 l1 i     = {-# SCC l1 #-} crossI (V.replicate @NMid (sigmoid . fixed neuron i))
 mnistNet i = {-# SCC mnistNet #-} l2 . ((dupI @NOut (V.foldr1 (V.zipWith (+))) . l1 i) × id) where n = fixed neuron i
 
-cost :: forall nexs a. (KnownNat nexs, Floating a) => V.Vector nexs (V.Vector NIn a, V.Vector NOut a) -> Weights (1+NIn) (1+NMid) a :-> a
+cost :: V.Vector BatchSize (V.Vector NIn Double, V.Vector NOut Double) -> Weights (1+NIn) (1+NMid) Double :-> Double
 cost  ps = {-# SCC cost #-} sumI . crossI (V.map cost1 ps) . dupI sum
-  where cost1 (i, o) = {-# SCC cost1 #-} fixed mul (fromIntegral $ natVal (Proxy @nexs)) . rec . sumI . crossI (V.replicate @NOut sqr) . (V.map (*(-1)) o +>) . mnistNet i
+  where cost1 (i, o) = {-# SCC cost1 #-} fixed mul (fromIntegral $ natVal (Proxy @BatchSize)) . rec . sumI . crossI (V.replicate @NOut sqr) . (V.map (*(-1)) o +>) . mnistNet i
         sqr = {-# SCC sqr #-} mul . dup
 
 type BatchSize = 32
 batchSize = 32
 
-step :: (Show a, Floating a) => V.Vector NExamples (V.Vector NIn a, V.Vector NOut a) -> Int
-     -> Weights (1+NIn) (1+NMid) a
-     -> IO (Weights (1+NIn) (1+NMid) a)
+step :: V.Vector NExamples (V.Vector NIn Double, V.Vector NOut Double) -> Int
+     -> Weights (1+NIn) (1+NMid) Double
+     -> IO (Weights (1+NIn) (1+NMid) Double)
 step examples i weights = {-# SCC step #-} do
   let off = (i * batchSize) `mod` (nExamples - batchSize)
       batch = fromJust $ V.toSized @BatchSize $ NV.slice off batchSize (V.fromSized examples)
@@ -219,63 +218,13 @@ instance Floating Expr where
   sqrt  = Sqrt
   pi    = Const pi
 
-symGrad :: IO ()
-symGrad = do
-  let (yS, Dual gS) = sigmoid # Var "x"
-  putStrLn $ "sigmoid(x)         = " ++ show yS
-  putStrLn $ "k * d/dx sigmoid x = " ++ show (gS (Var "k"))
-  putStrLn $ "k * d/dx sigmoid x = " ++ show (gS (Var "k"))
-  let sqr = mul . dup
-      (yQ, Dual gQ) = sqr # Var "x"
-  putStrLn $ "x*x                = " ++ show yQ
-  putStrLn $ "k * d/dx (x*x)     = " ++ show (gQ (Var "k"))
-
-size :: Expr -> Int
-size (Const _) = 1
-size (Var _)   = 1
-size (Add a b) = 1 + size a + size b
-size (Mul a b) = 1 + size a + size b
-size (Neg a)   = 1 + size a
-size (Rec a)   = 1 + size a
-size (Exp a)   = 1 + size a
-size (Sqrt a)  = 1 + size a
-
-eval :: (String -> Double) -> Expr -> Double
-eval _env (Const d) = d
-eval  env (Var x)   = env x
-eval  env (Add a b) = eval env a + eval env b
-eval  env (Mul a b) = eval env a * eval env b
-eval  env (Neg a)   = negate (eval env a)
-eval  env (Rec a)   = recip (eval env a)
-eval  env (Exp a)   = exp (eval env a)
-eval  env (Sqrt a)  = sqrt (eval env a)
-
-lookupVec :: (V.Vector 784 Double, V.Vector 10 Double) -> String -> Double
-lookupVec (ins,out) ('x':is) = V.index ins (finite @784 (read is))
-lookupVec (ins,out) ('y':is) = V.index out (finite @10 (read is))
-
--- symGradCostSize :: IO ()
--- symGradCostSize = do
-symGradCostSize = do
-  rawImages <- BS.drop 16 <$> BS.readFile "train-images.idx3-ubyte"
-  rawLabels <- BS.drop 8  <$> BS.readFile "train-labels.idx1-ubyte"
-  let toV bs  = NV.generate (BS.length bs) (fromIntegral @_ @Double . BS.index bs)
-      allImgs = toV rawImages
-      allLbls = toV rawLabels
-      examples = fromJust $ V.fromList @NExamples $
-        [ ( fromJust $ V.toSized @NIn $ NV.slice (i * nIn) nIn allImgs
-          , labelToVec (allLbls NV.! i) )
-        | i <- [0..nExamples-1] ]
-      example = (V.generate @NIn (\i -> Var $ 'x':show i), V.generate @NOut (\i -> Var $ 'y':show i))
-  weights <- (,) <$> V.replicateM @NMid (V.replicateM @(1+NIn) $ Const <$> randomM globalStdGen) <*> V.replicateM @NOut (V.replicateM @(1+NMid) $ Const <$> randomM globalStdGen)
-  let
-      (r, Dual g) = cost (V.singleton example) # weights
-      (gw1, gw2) = g (Const 1)
-      report name e =
-        let s0 = size e
-        in do
-          putStrLn $ name ++ "(size: " ++ show s0 ++ "): " ++ take 10000 (show e) -- (eval (lookupVec (head examples)) e)
-          -- putStrLn $ name ++ ".EVAL: " ++ show (eval (lookupVec (V.head examples)) e)
-  -- report "full cost            " r
-  report "d cost / d ws1[0][0]" (V.index (V.index gw1 (finite 0)) (finite 0))
-  -- report "d cost / d ws2[0][0]" (gw2 V.! 0 V.! 0)
+-- symGrad :: IO ()
+-- symGrad = do
+--   let (yS, Dual gS) = sigmoid # Var "x"
+--   putStrLn $ "sigmoid(x)         = " ++ show yS
+--   putStrLn $ "k * d/dx sigmoid x = " ++ show (gS (Var "k"))
+--   putStrLn $ "k * d/dx sigmoid x = " ++ show (gS (Var "k"))
+--   let sqr = mul . dup
+--       (yQ, Dual gQ) = sqr # Var "x"
+--   putStrLn $ "x*x                = " ++ show yQ
+--   putStrLn $ "k * d/dx (x*x)     = " ++ show (gQ (Var "k"))
