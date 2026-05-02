1400b1583a360ba8b4f07b36f8b0647dc18cba5c
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-02 09:54:17 +0100

    Symbolic AD

--- orgfiles/code/simple-essence-neural-net/mnist/MNIST.hs
+++ orgfiles/code/simple-essence-neural-net/mnist/MNIST.hs
@@ -35,28 +35,24 @@ exp'      = D $ \x -> let e = exp x in (e, scale e)
 sqrt'     = D $ \x -> let r = sqrt x in (r, scale (r*(1/2)))
 --------------------------------------------------------------------------------
 dupI join n = linear (\x -> V.replicate n x) (Dual join)
-crossI :: V.Vector (b :-> a) -> UV.Vector b :-> UV.Vector a
-crossI fs = D $ \as -> let (bs, bsas) = V.unzip (V.zipWith (#) fs (UV.convert as)) in (UV.convert bs, Dual (UV.convert . V.zipWith (<|) bsas . UV.convert))
-sumI      = D $ \xs -> (sum xs, Dual (\x -> UV.replicate (length xs) x))
-hadamard  = D $ \(ss, xs) -> (ss .*. xs, Dual (\dfs -> (xs .*. dfs, ss .*. dfs))) where (.*.) = UV.zipWith (*)
+crossI fs = D $ \as -> let (bs, bsas) = V.unzip (V.zipWith (#) fs as) in (bs, Dual (V.zipWith (<|) bsas))
+sumI      = D $ \xs -> (sum xs, Dual (\x -> V.replicate (length xs) x))
+hadamard  = D $ \(ss, xs) -> (ss .*. xs, Dual (\dfs -> (xs .*. dfs, ss .*. dfs))) where (.*.) = V.zipWith (*)
 fixed f a = D $ \b -> let (c, Dual d) = f # (a, b) in (c, Dual (snd . d))
-cons    x = D $ \xs -> (x `UV.cons` xs, Dual (\dxs -> UV.drop 1 dxs)) -- add a constant number to head of Vec. All weight vecs have leading biases
+cons    x = D $ \xs -> (x `V.cons` xs, Dual (\dxs -> V.drop 1 dxs)) -- add a constant number to head of Vec. All weight vecs have leading biases
 --------------------------------------------------------------------------------
 sigmoid  = rec . (1 +>) . exp' . neg -- 1/(1+exp(-x))
 neuron   = sigmoid . (sumI . hadamard) . (cons 1 × id)
-l2 :: (V.Vector (UV.Vector Double), V.Vector (UV.Vector Double)) :-> UV.Vector Double
-l2 = crossI (V.replicate 10 neuron) . (linear (\(xs,ys) -> UV.convert $ V.zipWith (,) xs ys) (Dual (UV.convert V.unzip)))
-l1 i = crossI (V.replicate 300 (fixed neuron i))
-mnistNet :: UV.Vector Double -> Weights :-> UV.Vector Double
-mnistNet i = l2 . ((dupI (V.foldr1 (UV.zipWith (+))) 10 . l1 i) × id) where n = fixed neuron i
-cost :: [(UV.Vector Double, UV.Vector Double)] -> Weights :-> Double
+l2       = crossI (V.replicate 10 neuron) . (linear (\(xs,ys) -> V.zipWith (,) xs ys) (Dual V.unzip))
+l1 i     = crossI (V.replicate 300 (fixed neuron i))
+mnistNet :: V.Vector Expr -> (V.Vector (V.Vector Expr), V.Vector (V.Vector Expr)) :-> V.Vector Expr
+mnistNet i = l2 . ((dupI (V.foldr1 (V.zipWith (+))) 10 . l1 i) × id) where n = fixed neuron i
+cost :: [(V.Vector Expr, V.Vector Expr)]
+     -> (V.Vector (V.Vector Expr), V.Vector (V.Vector Expr)) :-> Expr
 cost  ps = sumI . crossI (V.map cost1 (V.fromList ps)) . dupI sum (length ps)
-  where cost1 (i, o) = sqrt' . sumI . crossI (V.replicate 10 sqr) . what o . mnistNet i
-        cost1 :: (UV.Vector Double, UV.Vector Double) -> Weights :-> Double
-        sqr :: Double :-> Double
+  where cost1 (i, o) = sqrt' . sumI . crossI (V.replicate 10 sqr) . (V.map (*(-1)) o +>) . mnistNet i
+        sqr :: Expr :-> Expr
         sqr = mul . dup
-        what :: UV.Vector b -> UV.Vector b :-> UV.Vector b
-        what o = (UV.map (*(-1)) o +>)
 
 step examples (i :: Int) weights = do
   let (r, Dual grad) = cost examples # weights
@@ -66,23 +62,71 @@ step examples (i :: Int) weights = do
 main = do
   rawImages <- BS.drop 16 <$> BS.readFile "train-images.idx3-ubyte"
   rawLabels <- BS.drop 8  <$> BS.readFile "train-labels.idx1-ubyte"
-  let n      = BS.length rawLabels
-      imgVec :: UV.Vector Double
-      imgVec  = UV.generate (n * 784) (\i -> fromIntegral (BS.index rawImages i))
-      examples :: [(UV.Vector Double, UV.Vector Double)]
-      examples = take 100 [ ( UV.generate 784 (\j -> imgVec UV.! (i * 784 + j))
-                   , UV.singleton (fromIntegral (BS.index rawLabels i))
-                   )
+  let n       = BS.length rawLabels
+      toV bs  = V.generate (BS.length bs) (Const . fromIntegral . BS.index bs)
+      allImgs = toV rawImages
+      allLbls = toV rawLabels
+      examples :: [(V.Vector Expr, V.Vector Expr)]
+      examples = [ ( V.slice (i * 784) 784 allImgs
+                   , V.singleton (allLbls V.! i) )
                  | i <- [0..n-1] ]
-  initialWeights <- (,) <$> V.mapM (const $ UV.replicateM 785 $ randomM globalStdGen) [1..300] <*> V.mapM (const $ UV.replicateM 301 $ randomM globalStdGen) [1..10]
+  initialWeights <- (,) <$> V.mapM (const $ V.replicateM 785 $ (Const <$> randomM globalStdGen)) [1..300] <*> V.mapM (const $ V.replicateM 301 $ (Const <$> randomM globalStdGen)) [1..10]
   finalWeights   <- V.foldl' (\acc i -> acc >>= step examples i) (pure initialWeights) [0..1000]
   putStrLn $ "Neural net results: " ++ show (map (\(e,_) -> fst (mnistNet e # finalWeights)) examples)
   putStrLn $ "Expected results:   " ++ show (map snd examples)
 
-type Weights = (V.Vector (UV.Vector Double), V.Vector (UV.Vector Double))
+type Weights = (V.Vector (V.Vector Expr), V.Vector (V.Vector Expr))
 instance Num Weights where
-  fromInteger x' = (V.replicate 300 (UV.replicate 785 x), V.replicate 10 (UV.replicate 301 x)) where x = fromInteger x'
-  (w1, w2) + (w3, w4) = (V.zipWith (UV.zipWith(+)) w1 w3, V.zipWith (UV.zipWith(+)) w2 w4)
-instance Num (UV.Vector Double) where
+  fromInteger x' = (V.replicate 300 (V.replicate 785 x), V.replicate 10 (V.replicate 301 x)) where x = fromInteger x'
+  (w1, w2) + (w3, w4) = (V.zipWith (V.zipWith(+)) w1 w3, V.zipWith (V.zipWith(+)) w2 w4)
+instance Num (V.Vector Expr) where
   -- the negate/fromInteger 0/fromInteger -1 was causing such a weird loop (also the `sum` from `dupI` needing a zero..) wow.
-  (+) = UV.zipWith (+)
+  (+) = V.zipWith (+)
+
+--------------------------------------------------------------------------------
+-- Symbolic ad
+--------------------------------------------------------------------------------
+
+data Expr = Const Double
+          | Var Char
+          | Add Expr Expr
+          | Mul Expr Expr
+          | Neg Expr
+          | Rec Expr
+          | Exp Expr
+          | Sqrt Expr
+
+instance Show Expr where
+  show (Const d) = show d
+  show (Var c)   = [c]
+  show (Add a b) = "(" ++ show a ++ " + " ++ show b ++ ")"
+  show (Mul a b) = "(" ++ show a ++ " * " ++ show b ++ ")"
+  show (Neg a)   = "(-" ++ show a ++ ")"
+  show (Rec a)   = "(1/" ++ show a ++ ")"
+  show (Exp a)   = "exp(" ++ show a ++ ")"
+  show (Sqrt a)  = "sqrt(" ++ show a ++ ")"
+
+instance Num Expr where
+  (+)         = Add
+  (*)         = Mul
+  negate      = Neg
+  fromInteger = Const . fromInteger
+
+instance Fractional Expr where
+  recip        = Rec
+  fromRational = Const . fromRational
+
+instance Floating Expr where
+  exp   = Exp
+  sqrt  = Sqrt
+  pi    = Const pi
+
+symGrad :: IO ()
+symGrad = do
+  let (yS, Dual gS) = (sigmoid :: Expr :-> Expr) # Var 'x'
+  putStrLn $ "sigmoid(x)         = " ++ show yS
+  putStrLn $ "k * d/dx sigmoid x = " ++ show (gS (Var 'k'))
+  let sqr = mul . dup
+      (yQ, Dual gQ) = (sqr :: Expr :-> Expr) # Var 'x'
+  putStrLn $ "x*x                = " ++ show yQ
+  putStrLn $ "k * d/dx (x*x)     = " ++ show (gQ (Var 'k'))
