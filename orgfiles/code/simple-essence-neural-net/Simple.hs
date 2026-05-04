2cc34dc87beadfa5dba959268c6e2bb407b8ba02
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-04 21:55:51 +0100

    pretty even more Simple.hs

--- orgfiles/code/simple-essence-neural-net/Simple.hs
+++ orgfiles/code/simple-essence-neural-net/Simple.hs
@@ -14,32 +14,31 @@ f × g = D $ \(a,b) ->
    in ((c,d), \(x,y) -> (f' x, g' y))
 
 ------------------------- Building Blocks --------------------------------------
-assoc    = D $ \(a,(b,c)) -> (((a,b),c), \((a,b),c) -> (a,(b,c)))
-dup      = D $ \x -> ((x,x), uncurry (+))          ; dup :: Num a => a :-> (a,a)
-add      = D $ \(x,y) -> (x + y, \x -> (x,x))
-mul      = D $ \(x,y) -> (x*y, \df -> (df*y,df*x))
-rec      = D $ \x -> (1/x, (*(-1 / x^2)))
-exp'     = D $ \x -> let e = exp x in (e, (*e))
-dot'     = D $ \(a,b) -> (sum (zipWith (*) a b), \d -> (map (*d) b, map (*d) a))
-map' f   = D $ \a -> let (b, f') = unzip (map (f #) a) in (b, zipWith ($) f')
-f `at` a = D $ \b -> let (c, d) = f # (a, b) in (c, snd . d)  -- papp static val
+assoc    = D (\(a,(b,c)) -> (((a,b),c), \((a,b),c) -> (a,(b,c))))
+dup      = D (\x -> ((x,x), uncurry (+)))          ; dup :: Num a => a :-> (a,a)
+add      = D (\(x,y) -> (x + y, \x -> (x,x)))
+mul      = D (\(x,y) -> (x*y, \df -> (df*y,df*x)))
+rec      = D (\x -> (1/x, (*(-1 / x^2))))
+exp'     = D (\x -> let e = exp x in (e, (*e)))
+dot'     = D (\(a,b) -> (sum (zipWith (*) a b), \d -> (map (*d) b, map (*d) a)))
+map' f   = D (\a -> let (b, f') = unzip (map (f #) a) in (b, zipWith ($) f'))
+f `at` a = D (\b -> let (c, d) = f # (a, b) in (c, snd . d))  -- papp static val
 
 ------------------------- Neural Network ---------------------------------------
-sigmoid        = rec . (add `at` 1) . exp' . (mul `at` (-1))     -- 1/(1+exp(-x))
+sigmoid        = rec . (add `at` 1) . exp' . (mul `at` (-1))    -- 1/(1+exp(-x))
 neuron         = sigmoid . add . (dot' × id) . assoc                 -- σ(W·I+b)
-xorNet i       = neuron . (map' (neuron `at` i) × id)
-cost1 (i, o)   = mul . dup . (add `at` (-o)) . xorNet i
+xornet i       = neuron . (map' (neuron `at` i) × id)        -- 2x4x1 neural net
+cost1 (i, o)   = mul . dup . (add `at` (-o)) . xornet i  -- sqr err cost of 1 ex
 cost [m,n,l,p] = add . (add × add) . ((cost1 m × cost1 n) × (cost1 l × cost1 p)) . (dup × dup) . dup
 
-step (i :: Int) weights = do
-  let (r, grad) = cost exs # weights
-  putStrLn $ "Cost(" ++ show i ++ "): " ++ show r
-  return $ weights + grad (-10)
+step i weights | let (r, grad) = cost exs # weights
+               = putStrLn ("Cost(" ++ show i ++ "): " ++ show r)
+               >> return (weights + grad (-10))
 
 exs@[(a,a'),(b,b'),(c,c'),(d,d')] = [([0,0],0), ([0,1],1), ([1,0],1), ([1,1],0)]
 main = do
   ws0 <- readLn @([([Double], Double)], ([Double], Double))      -- read weights
   ws1 <- foldl' (\acc i -> acc >>= step i) (pure ws0) [0..99999] -- upd. weights
-  print $ map (\(i,o) -> ((fst (xorNet i # ws1)), " ~= ", show o)) exs
+  print $ map (\(i,o) -> show (fst (xornet i # ws1)) ++ " ~=? " ++ show o) exs
 
 instance Num a => Num ([a], a) where (w,b) + (v,c) = (zipWith (+) w v, b + c)
