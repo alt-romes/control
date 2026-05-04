916c916f9f979c5e5eab1f36209cb2b8e8d3c807
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-04 22:00:51 +0100

    ALL <=80 COLS
    without losing lines nor whitespace.
    Clever clever..

--- orgfiles/code/simple-essence-neural-net/Simple.hs
+++ orgfiles/code/simple-essence-neural-net/Simple.hs
@@ -13,7 +13,7 @@ f × g = D $ \(a,b) ->
   let (c, f') = f # a; (d, g') = g # b
    in ((c,d), \(x,y) -> (f' x, g' y))
 
-------------------------- Building Blocks --------------------------------------
+----------------------- Primitive functions ------------------------------------
 assoc    = D (\(a,(b,c)) -> (((a,b),c), \((a,b),c) -> (a,(b,c))))
 dup      = D (\x -> ((x,x), uncurry (+)))          ; dup :: Num a => a :-> (a,a)
 add      = D (\(x,y) -> (x + y, \x -> (x,x)))
@@ -29,8 +29,8 @@ sigmoid        = rec . (add `at` 1) . exp' . (mul `at` (-1))    -- 1/(1+exp(-x))
 neuron         = sigmoid . add . (dot' × id) . assoc                 -- σ(W·I+b)
 xornet i       = neuron . (map' (neuron `at` i) × id)        -- 2x4x1 neural net
 cost1 (i, o)   = mul . dup . (add `at` (-o)) . xornet i  -- sqr err cost of 1 ex
-cost [m,n,l,p] = add . (add × add) . ((cost1 m × cost1 n) × (cost1 l × cost1 p)) . (dup × dup) . dup
-
+cost [m,n,l,p] = add . (add × add) . ((cost1 m × cost1 n) × (cost1 l × cost1 p))
+                                                             . (dup × dup) . dup
 step i weights | let (r, grad) = cost exs # weights
                = putStrLn ("Cost(" ++ show i ++ "): " ++ show r)
                >> return (weights + grad (-10))
@@ -38,7 +38,7 @@ step i weights | let (r, grad) = cost exs # weights
 exs@[(a,a'),(b,b'),(c,c'),(d,d')] = [([0,0],0), ([0,1],1), ([1,0],1), ([1,1],0)]
 main = do
   ws0 <- readLn @([([Double], Double)], ([Double], Double))      -- read weights
-  ws1 <- foldl' (\acc i -> acc >>= step i) (pure ws0) [0..99999] -- upd. weights
+  ws1 <- foldl' (\ws' i -> ws' >>= step i) (pure ws0) [0..99999] -- upd. weights
   print $ map (\(i,o) -> show (fst (xornet i # ws1)) ++ " ~=? " ++ show o) exs
 
 instance Num a => Num ([a], a) where (w,b) + (v,c) = (zipWith (+) w v, b + c)
