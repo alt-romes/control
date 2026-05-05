4576037bfffc2fa4f7153af2b1535967cb173aa1
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-05 23:07:58 +0100

    experiment w sections in Simple.hs

--- orgfiles/code/simple-essence-neural-net/Simple.hs
+++ orgfiles/code/simple-essence-neural-net/Simple.hs
@@ -12,6 +12,7 @@ instance Category (:->) where
 f × g = D $ \(a,b) ->
   let (c, f') = f # a; (d, g') = g # b
    in ((c,d), \(x,y) -> (f' x, g' y))
+--------------------------------------------------------------------------------
 
 ----------------------- Primitive functions ------------------------------------
 assoc    = D (\(a,(b,c)) -> (((a,b),c), \((a,b),c) -> (a,(b,c))))
@@ -23,6 +24,7 @@ exp'     = D (\x -> let e = exp x in (e, (*e)))
 dot'     = D (\(a,b) -> (sum (zipWith (*) a b), \d -> (map (*d) b, map (*d) a)))
 map' f   = D (\a -> let (b, f') = unzip (map (f #) a) in (b, zipWith ($) f'))
 f `at` a = D (\b -> let (c, d) = f # (a, b) in (c, snd . d))  -- papp static val
+--------------------------------------------------------------------------------
 
 ------------------------- Neural Network ---------------------------------------
 sigmoid        = rec . (add `at` 1) . exp' . (mul `at` (-1))    -- 1/(1+exp(-x))
@@ -34,11 +36,14 @@ cost [m,n,l,p] = add . (add × add) . ((cost1 m × cost1 n) × (cost1 l × cost1
 step i weights | let (r, grad) = cost exs # weights
                = putStrLn ("Cost(" ++ show i ++ "): " ++ show r)
                >> return (weights + grad (-10))
+--------------------------------------------------------------------------------
 
+----------------------------- Main ---------------------------------------------
 exs@[(a,a'),(b,b'),(c,c'),(d,d')] = [([0,0],0), ([0,1],1), ([1,0],1), ([1,1],0)]
 main = do
   ws0 <- readLn @([([Double], Double)], ([Double], Double))      -- read weights
   ws1 <- foldl' (\ws' i -> ws' >>= step i) (pure ws0) [0..99999] -- upd. weights
   print $ map (\(i,o) -> show (fst (xornet i # ws1)) ++ " ~=? " ++ show o) exs
+--------------------------------------------------------------------------------
 
 instance Num a => Num ([a], a) where (w,b) + (v,c) = (zipWith (+) w v, b + c)
