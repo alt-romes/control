b905a3aebf2944b1e621e36d6478ae76d5cbae04
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-09 21:42:51 +0100

    fixes shortcuts.nix

--- nix/packages/shortcuts/shortcuts.nix
+++ nix/packages/shortcuts/shortcuts.nix
@@ -1,45 +1,46 @@
-{
-  flake.packages.aarch64-darwin = { lib, pkgs }:
-  let
-    # Get all the *.shortcut files from the folder
-    shortcutsDir = ./.;
-    shortcutFiles = builtins.filter (f: lib.hasSuffix ".shortcut" f)
-                      (builtins.attrNames (builtins.readDir shortcutsDir));
+{ inputs, ... }:
+let
+  pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
+  lib = pkgs.lib;
 
-    mkShortcutDeriv = fileName:
-      let
-        # Drop .shortcut suffix
-        shortcutName = "romes: ${lib.removeSuffix ".shortcut" fileName}";
-        binName = lib.strings.toLower (lib.strings.sanitizeDerivationName shortcutName);
-      in
-      pkgs.writeShellScriptBin binName ''
-          if ! shortcuts list | grep -Fxq "${shortcutName}"; then
-            echo "Shortcut '${shortcutName}' not found. Trying to install from source..." >&2
-            echo "Installing from backup version: \"${shortcutsDir}/${fileName}\"."
-            echo "Please confirm this shortcut is up to date with what it should be doing."
-            echo "--------------------------------------------------------------------------------"
-            echo "IMPORTANT: Please rename the shortcut to add the 'romes: ' suffix."
-            echo "--------------------------------------------------------------------------------"
-            open "${shortcutsDir}/${fileName}"
-            read -p "Press any key to continue" -n 1 -s
-          fi
+  # Get all the *.shortcut files from the folder
+  shortcutsDir = ./.;
+  shortcutFiles = builtins.filter (f: lib.hasSuffix ".shortcut" f)
+                    (builtins.attrNames (builtins.readDir shortcutsDir));
 
-          if [ -z "$1" ]; then
-            shortcuts run "${shortcutName}"
+  mkShortcutDeriv = fileName:
+    let
+      # Drop .shortcut suffix
+      shortcutName = "romes: ${lib.removeSuffix ".shortcut" fileName}";
+      binName = lib.strings.toLower (lib.strings.sanitizeDerivationName shortcutName);
+    in
+    pkgs.writeShellScriptBin binName ''
+        if ! shortcuts list | grep -Fxq "${shortcutName}"; then
+          echo "Shortcut '${shortcutName}' not found. Trying to install from source..." >&2
+          echo "Installing from backup version: \"${shortcutsDir}/${fileName}\"."
+          echo "Please confirm this shortcut is up to date with what it should be doing."
+          echo "--------------------------------------------------------------------------------"
+          echo "IMPORTANT: Please rename the shortcut to add the 'romes: ' suffix."
+          echo "--------------------------------------------------------------------------------"
+          open "${shortcutsDir}/${fileName}"
+          read -p "Press any key to continue" -n 1 -s
+        fi
+
+        if [ -z "$1" ]; then
+          shortcuts run "${shortcutName}"
+        else
+          if [ -f "$1" ]; then
+            shortcuts run "${shortcutName}" -i "$1"
           else
-            if [ -f "$1" ]; then
-              shortcuts run "${shortcutName}" -i "$1"
-            else
-              SHORTCUT_INPUT_TMP="$(mktemp -d)/input"
-              echo "$1" > "$SHORTCUT_INPUT_TMP"
-              shortcuts run "${shortcutName}" -i "$SHORTCUT_INPUT_TMP"
-            fi
+            SHORTCUT_INPUT_TMP="$(mktemp -d)/input"
+            echo "$1" > "$SHORTCUT_INPUT_TMP"
+            shortcuts run "${shortcutName}" -i "$SHORTCUT_INPUT_TMP"
           fi
-      '';
-  in
-  {
-    inherit mkShortcutDeriv;
-  } // builtins.listToAttrs (map (fileName: {
+        fi
+    '';
+in
+{
+  flake.packages.aarch64-darwin = builtins.listToAttrs (map (fileName: {
       name = "Shortcut-" + lib.strings.sanitizeDerivationName (lib.removeSuffix ".shortcut" fileName);
       value = mkShortcutDeriv fileName;
     }) shortcutFiles);
