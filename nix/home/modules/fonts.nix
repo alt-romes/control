945b9e9ba2fcb611df837d5d529e559087836a75
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-18 16:29:05 +0100

    font options

--- nix/home/modules/fonts.nix
+++ nix/home/modules/fonts.nix
@@ -22,6 +22,78 @@
           ghostty = "Fira Code";
           package = pkgs.fira-code;
         };
+        cascadia-code = {
+          ghostty = "CaskaydiaMono Nerd Font";
+          package = pkgs.nerd-fonts.caskaydia-mono;
+        };
+        victor-mono = {
+          ghostty = "VictorMono Nerd Font";
+          package = pkgs.nerd-fonts.victor-mono;
+        };
+        hack = {
+          ghostty = "Hack Nerd Font";
+          package = pkgs.nerd-fonts.hack;
+        };
+        commit-mono = {
+          ghostty = "CommitMono Nerd Font";
+          package = pkgs.nerd-fonts.commit-mono;
+        };
+        geist-mono = {
+          ghostty = "GeistMono Nerd Font";
+          package = pkgs.nerd-fonts.geist-mono;
+        };
+        zed-mono = {
+          ghostty = "ZedMono Nerd Font";
+          package = pkgs.nerd-fonts.zed-mono;
+        };
+        departure-mono = {
+          ghostty = "DepartureMono Nerd Font";
+          package = pkgs.nerd-fonts.departure-mono;
+        };
+        monaspace-neon = {
+          ghostty = "Monaspace Neon";
+          package = pkgs.monaspace;
+        };
+        monaspace-argon = {
+          ghostty = "Monaspace Argon";
+          package = pkgs.monaspace;
+        };
+        monaspace-krypton = {
+          ghostty = "Monaspace Krypton";
+          package = pkgs.monaspace;
+        };
+        monaspace-xenon = {
+          ghostty = "Monaspace Xenon";
+          package = pkgs.monaspace;
+        };
+        monaspace-radon = {
+          ghostty = "Monaspace Radon";
+          package = pkgs.monaspace;
+        };
+        intel-one-mono = {
+          ghostty = "Intel One Mono";
+          package = pkgs.intel-one-mono;
+        };
+        recursive-mono = {
+          ghostty = "RecMonoCasual Nerd Font";
+          package = pkgs.nerd-fonts.recursive-mono;
+        };
+        martian-mono = {
+          ghostty = "MartianMono Nerd Font";
+          package = pkgs.nerd-fonts.martian-mono;
+        };
+        mononoki = {
+          ghostty = "Mononoki Nerd Font";
+          package = pkgs.nerd-fonts.mononoki;
+        };
+        fantasque-sans-mono = {
+          ghostty = "FantasqueSansM Nerd Font Mono";
+          package = pkgs.nerd-fonts.fantasque-sans-mono;
+        };
+        lilex = {
+          ghostty = "Lilex";
+          package = pkgs.lilex;
+        };
       };
 
       font = fontName: fontConf:
