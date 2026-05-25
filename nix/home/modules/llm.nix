b66b2b0c75b1495444c7242121f4813476c8abf2
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-05-25 21:16:02 +0100

    no hadrian skill; I'm using hadrian-util now

--- nix/home/modules/llm.nix
+++ nix/home/modules/llm.nix
@@ -41,64 +41,6 @@
     
         context = aiContext;
     
-        skills = {
-          hadrian_test = ''
-            ---
-            name: hadrian
-            description: Hadrian is GHC's build system. You should run hadrian to build GHC and to test specific tests from the GHC testsuite
-            ---
-    
-            The default hadrian (`./hadrian/build`) GHC build system is wrapped as a shell function `hadrian`:
-            To build GHC from scratch run
-            ```
-            hadrian
-            ```
-    
-            This will re-use the existing tree flavour. If no flavour is already defined, use by default:
-            ```
-            hadrian --flavour=perf+no_profiled_libs+debug_ghc+debug_info
-            ```
-    
-            If you want to fix stage1, run with
-            ```
-            hadrian <flavour if needed> --freeze1
-            ```
-    
-            ## Running a test
-    
-            To run a specific test, first build the tree as described above.
-            The flavour will be recorded and then you can run the test through
-            hadrian first to get a result:
-            ```
-            hadrian --freeze1 test --only="T12345"
-            ```
-    
-            Once the test runs the first time, run it again with additional flags
-            to preserve temporary files and print out the direct command line invocation:
-            ```
-            hadrian --freeze1 test --only="<test>" -k -V
-            ```
-    
-            From the output of the previous command, you should capture the command
-            line invocation (it will look like `cd <testsuite directory> &&
-            .../_build/stage1/bin/ghc <args>`).
-    
-            Now, run that ghc invocation directly. Make sure to remove
-            `-dno-debug-output` and other flags which suppress output to ensure the
-            compiler output is visible.
-    
-            ## Profiling GHC while running a test
-    
-            If the compiler flavour (consult `_build/flavour.txt` in the GHC root)
-            is profiled (includes `+profiled_ghc`), then you can run the ghc
-            invocation with additional flags `+RTS -pj -RTS` to produce a JSON
-            `*.prof` profile file which can be visualized as a flamegraph in
-            speedscope.app. You can also use `+RTS -p -RTS` to produce a `.prof`
-            file which uses a GHC format for displaying the same information in a
-            more human readable way but which can still be read by a machine.
-            '';
-        };
-    
         settings = {
           model = "gpt-5.4";
           model_reasoning_effort = "medium";
