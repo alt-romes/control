a4562ead7d177f90328c1f90af7c5dfaee7582ec
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-06-13 13:57:08 +0100

    improve nix derivation for gitlab-index

--- nix/packages/gitlab-index/gitlab-index.nix
+++ nix/packages/gitlab-index/gitlab-index.nix
@@ -1,28 +1,28 @@
 { inputs, ... }:
-let
-  pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
-  lib = pkgs.lib;
-  hpkgs = pkgs.haskellPackages;
-
-  # cabal2nix derivation; justStaticExecutables keeps GHC out of the runtime
-  # closure (so this is a small leaf package, not a Haskell dev env).
-  unwrapped = pkgs.haskell.lib.justStaticExecutables
-    (hpkgs.callCabal2nix "gitlab-index" ./. { });
-
-  # Runtime CLIs the tool shells out to.
-  runtimeDeps = [ pkgs.glab pkgs.fzf pkgs.glow pkgs.ripgrep ];
-
-  gitlab-index = pkgs.runCommand "gitlab-index"
-    {
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      meta = unwrapped.meta // { mainProgram = "gitlab-index"; };
-    } ''
-    mkdir -p $out/bin
-    makeWrapper ${unwrapped}/bin/gitlab-index $out/bin/gitlab-index \
-      --set GITLAB_INDEX_SELF $out/bin/gitlab-index \
-      --prefix PATH : ${lib.makeBinPath runtimeDeps}
-  '';
-in
 {
-  flake.packages.aarch64-darwin.gitlab-index = gitlab-index;
+  perSystem = { pkgs, lib, ... }: {
+    packages.gitlab-index =
+    let
+      hpkgs = pkgs.haskellPackages;
+
+      # cabal2nix derivation; justStaticExecutables keeps GHC out of the runtime
+      # closure (so this is a small leaf package, not a Haskell dev env).
+      unwrapped = pkgs.haskell.lib.justStaticExecutables
+        (hpkgs.callCabal2nix "gitlab-index" ./. { });
+
+      # Runtime CLIs the tool shells out to.
+      runtimeDeps = [ pkgs.glab pkgs.fzf pkgs.glow pkgs.ripgrep ];
+
+      gitlab-index = pkgs.runCommand "gitlab-index"
+        {
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          meta = unwrapped.meta // { mainProgram = "gitlab-index"; };
+        } ''
+        mkdir -p $out/bin
+        makeWrapper ${unwrapped}/bin/gitlab-index $out/bin/gitlab-index \
+          --set GITLAB_INDEX_SELF $out/bin/gitlab-index \
+          --prefix PATH : ${lib.makeBinPath runtimeDeps}
+      '';
+    in gitlab-index;
+  };
 }
